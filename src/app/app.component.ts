4558256f5ffd705539f3befdeb858781ee8aadc9
GBurgardt <germanandresburgardt@gmail.com>
2019-06-14 13:17:18 -0300

    Fi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'admin-app';
+    spName: string = 'puntosInformados 1';
+
+    columns: any;
+    data: any[];
+
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    execSp = () => this.http.get(`${environment.WS_URL}/admin-sp/${this.spName}`).toPromise()
+        .then(
+            (resp: any) => {
+                if (resp && resp.length > 0) {
+                    this.columns = Object.keys(resp[0])
+                    this.data = resp;
+                } else {
+                    alert(resp)
+                }
+            }
+        )
+        .catch(
+            err => alert(err)
+        )
+    
+    getColumns = () => Object.values(this.columns)
 }
