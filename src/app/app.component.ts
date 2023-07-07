837d9655e976f8e3be47d430793b43ae86e536b0
Cristian Martínez DUrbano <cristian@ahoradoctor.com>
2023-07-07 17:32:19 -0300

    Primer actualizacion - Elecciones 2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,42 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-    spName: string = 'puntosInformados 1';
+    spName: string = "puntosInformados 1";
+    error: any;
 
     columns: any;
     data: any[];
 
-    constructor(
-        private http: HttpClient
-    ) { }
+    constructor(private http: HttpClient) {}
 
-    execSp = () => this.http.get(`${environment.WS_URL}/admin-sp/${this.spName}`).toPromise()
-        .then(
-            (resp: any) => {
+    execSp = () => {
+        this.error = undefined;
+        this.columns = undefined;
+        this.data = undefined;
+        this.http
+            .get(`${environment.WS_URL}/admin-sp/${this.spName}`)
+            .toPromise()
+            .then((resp: any) => {
                 if (resp && resp.length > 0) {
-                    this.columns = Object.keys(resp[0])
+                    this.columns = Object.keys(resp[0]);
                     this.data = resp;
                 } else {
-                    alert(resp)
+                    // alert(JSON.stringify(resp));
+                    this.error = resp;
                 }
-            }
-        )
-        .catch(
-            err => alert(err)
-        )
-    
-    getColumns = () => Object.values(this.columns)
+            })
+            .catch((err) => {
+                // alert(JSON.stringify(err));
+                this.error = err;
+            });
+    };
+
+    getColumns = () => Object.values(this.columns);
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    WS_URL: 'http://appsantafe.myddns.me:3001'
+    WS_URL: 'http://api.mesastesti.com.ar:3001'
     // WS_URL: 'http://localhost:3001'
 };
